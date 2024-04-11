51ea776b974118c059b7d370f6f8df2b0bc21111
korvalanchik <korvalan@yahoo.com>
2024-04-11 23:27:57 +0300

    Init shortener with migrations

---
+++ src/main/java/com/example/highhopes/shortlink/ShortLink.java
@@ -0,0 +1,57 @@
+package com.example.highhopes.shortlink;
+
+import com.example.highhopes.user.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+
+
+@Entity
+@Table(name = "short_links")
+@Getter
+@Setter
+public class ShortLink {
+
+    @Id
+    @SequenceGenerator(
+            name = "primary_sequence",
+            sequenceName = "primary_sequence",
+            allocationSize = 1,
+            initialValue = 10000
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "primary_sequence"
+    )
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "original_url", nullable = false)
+    private String originalUrl;
+
+    @Column(name = "short_url", nullable = false, unique = true)
+    private String shortUrl;
+
+    @Column(name = "creation_date", nullable = false)
+    private OffsetDateTime creationDate;
+
+    @Column(name = "expiry_date")
+    private OffsetDateTime expiryDate;
+
+    @Column(name = "status", nullable = false)
+    private boolean active;
+
+    @Column(name = "clicks", nullable = false)
+    private int clicks;
+
+    @PrePersist
+    public void prePersist() {
+        this.creationDate = OffsetDateTime.now();
+    }
+
+}
---
+++ src/main/java/com/example/highhopes/user/User.java
@@ -0,0 +1,64 @@
+package com.example.highhopes.user;
+
+import com.example.highhopes.shortlink.ShortLink;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.OffsetDateTime;
+import java.util.Set;
+
+
+@Entity
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @Column(nullable = false, updatable = false)
+    @SequenceGenerator(
+            name = "primary_sequence",
+            sequenceName = "primary_sequence",
+            allocationSize = 1,
+            initialValue = 10000
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "primary_sequence"
+    )
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user")
+    private Set<ShortLink> shortLinks;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private OffsetDateTime dateCreated;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private OffsetDateTime lastUpdated;
+
+    @PrePersist
+    public void prePersist() {
+        this.dateCreated = OffsetDateTime.now();
+        this.lastUpdated = OffsetDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.lastUpdated = OffsetDateTime.now();
+    }
+
+}
