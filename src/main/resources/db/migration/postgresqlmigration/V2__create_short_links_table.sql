f0e755a6d2bae1deddb7432639398a1866f14962
Victlion <victlionpro@gmail.com>
2024-04-24 18:48:55 +0300

    fix link url

--- src/main/resources/db/migration/postgresqlmigration/V2__create_short_links_table.sql
+++ src/main/resources/db/migration/postgresqlmigration/V2__create_short_links_table.sql
@@ -16,6 +16,6 @@ CREATE TABLE IF NOT EXISTS short_links (
 -- Inserting sample data into short_links table
 INSERT INTO short_links (id, user_id, original_url, short_url, creation_date, expiry_date, status, clicks)
 VALUES
-    (1, 2, 'https://app.slack.com/client/T060L949SJY/C06TVJGDCPK', 'http://localhost:8080/sl/abc123', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP + INTERVAL '7' DAY, TRUE, 4),
-    (2, 1, 'https://app.slack.com/client/T060L949SJY/C060L94BZAQ', 'http://localhost:8080/sl/def456', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP + INTERVAL '7' DAY, TRUE, 6),
-    (3, 1, 'https://app.slack.com/client/T060L949SJY/C06F4SA1B55', 'http://localhost:8080/sl/qwe457', TIMESTAMP '2024-03-20 12:34:56', TIMESTAMP '2024-03-27 12:34:56', TRUE, 6);
+    (1, 2, 'https://app.slack.com/client/T060L949SJY/C06TVJGDCPK', 'abc123', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP + INTERVAL '7' DAY, TRUE, 4),
+    (2, 1, 'https://app.slack.com/client/T060L949SJY/C060L94BZAQ', 'def456', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP + INTERVAL '7' DAY, TRUE, 6),
+    (3, 1, 'https://app.slack.com/client/T060L949SJY/C06F4SA1B55', 'qwe457', TIMESTAMP '2024-03-20 12:34:56', TIMESTAMP '2024-03-27 12:34:56', TRUE, 6);
